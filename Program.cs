a90b8bb97b6702de0ff29b6dfadbe4d2908fcbf3
Fadi Mriri <fmriri2@gmail.com>
2026-05-02 16:01:00 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -124,5 +124,12 @@ app.UseStaticFiles();
 
 app.MapControllers();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Run($"http://0.0.0.0:{port}");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+
+
+app.MapGet("/", () => "OK");
+
+app.Run();
+
